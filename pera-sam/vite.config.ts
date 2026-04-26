b4785ccba8ef0759ca52f3fe45d61ce0c405a5fe
zerokali20 <2003zerokali@gmail.com>
2026-04-26 14:27:22 +0530

    Fix issue azure devops

--- pera-sam/vite.config.ts
+++ pera-sam/vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
